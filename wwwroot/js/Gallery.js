ca95ee2ef847332491324b2191434ccbf8bc9453
Sander Broos <sander.broos.0@gmail.com>
2025-01-12 16:30:41 +0100

    Add tsconfig for compile on save option

--- wwwroot/js/Gallery.js
+++ wwwroot/js/Gallery.js
@@ -1,35 +1,41 @@
 var $;
-class Photo {
-    constructor(photo) {
+var Photo = /** @class */ (function () {
+    function Photo(photo) {
         this.ID = photo.ID;
         this.ThumbnailURL = photo.ThumbnailURL;
         this.Date = new Date(photo.Date);
     }
-}
-class Gallery {
-    constructor(photos) {
+    return Photo;
+}());
+var Gallery = /** @class */ (function () {
+    function Gallery(photos) {
+        var _this = this;
         this.currentYear = 2023;
         this.photos = [];
         if (photos != null) {
-            for (let thePhoto of photos)
+            for (var _i = 0, photos_1 = photos; _i < photos_1.length; _i++) {
+                var thePhoto = photos_1[_i];
                 this.photos.push(new Photo(thePhoto));
+            }
         }
-        $('#myButton2023').click(() => this.setYear(2023));
-        $('#myButton2024').click(() => this.setYear(2024));
+        $('#myButton2023').click(function () { return _this.setYear(2023); });
+        $('#myButton2024').click(function () { return _this.setYear(2024); });
         this.setYear(2023);
     }
-    setYear(year) {
+    Gallery.prototype.setYear = function (year) {
         this.currentYear = year;
-        let theGrid = $('#myImageGrid').empty();
+        var theGrid = $('#myImageGrid').empty();
         if (this.photos != null) {
-            for (let thePhoto of this.photos) {
+            for (var _i = 0, _a = this.photos; _i < _a.length; _i++) {
+                var thePhoto = _a[_i];
                 if (thePhoto.Date.getUTCFullYear() != this.currentYear)
                     continue;
-                let theImageItem = $('#myImageItemTemplate').children().clone();
+                var theImageItem = $('#myImageItemTemplate').children().clone();
                 theImageItem.find('img').addBack().attr('src', thePhoto.ThumbnailURL);
                 theGrid.append(theImageItem);
             }
         }
-    }
-}
+    };
+    return Gallery;
+}());
 //# sourceMappingURL=Gallery.js.map
